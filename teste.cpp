235256459b14ec0cf9c30b98b0a2725d98f0a17d
Samuel Luiz Souza <samuelluizsouza771@gmail.com>
2024-10-02 02:42:27 -0300

    little teste

---
+++ teste.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+    
+using namespace std;
+
+int main()
+{
+    cout<<"testando pq o puto do github não deixa eu clonar o repostorio do outro projeto";
+    
+    return 0;
+}
